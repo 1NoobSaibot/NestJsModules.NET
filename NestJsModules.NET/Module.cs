de65d13e7faad56e993a166ac057f78b0195858b
Evhen Diulin <zheka.diulin@gmail.com>
2023-02-07 11:06:05 +0200

    Init

---
+++ NestJsModules.NET/Module.cs
@@ -0,0 +1,31 @@
+﻿namespace NestJsModules
+{
+	public class Module
+	{
+		private Dictionary<string, object?> _singletoneScope = new Dictionary<string, object?>();
+
+
+		public void Add<T>(string key, T value)
+		{
+			_singletoneScope[key] = value;
+		}
+
+
+		public void Add<T>(T? obj)
+		{
+			_singletoneScope.Add(typeof(T).ToString(), obj);
+		}
+
+
+		public T? Get<T>(string key)
+		{
+			return (T?)_singletoneScope[key];
+		}
+
+
+		public T? Get<T>()
+		{
+			return (T?)_singletoneScope[typeof(T).ToString()];
+		}
+	}
+}
